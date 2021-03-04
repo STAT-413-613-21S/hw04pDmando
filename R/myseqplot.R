e9fae82aa8cc99ee84c37ca722e8a231710cfe15
Dmando <dleshchiner@gmail.com>
2021-03-03 22:08:58 -0500

    continue

--- R/myseqplot.R
+++ R/myseqplot.R
@@ -7,7 +7,18 @@
 #' @return Plot of the recursive function output.
 #' @export myseqplot
 #' @importFrom ggplot2 ggplot aes geom_line
-#' @examples myseqplot(inputdata)
+#' @examples my_data <- tibble::tribble(
+#' ~x, ~y, ~z, ~n,
+#' 2,4,3,3,
+#' 2,4,3,4,
+#' 2,4,3,5,
+#' 2,4,3,6,
+#' 2,4,3,7,
+#' 2,4,3,8,
+#' 2,4,3,9,
+#' 2,4,3,10,
+#' 2,4,3,12)
+#' myseqplot(my_data)
 
 myseqplot <- function(inputdata) {
   if (ncol(inputdata) != 4) {
